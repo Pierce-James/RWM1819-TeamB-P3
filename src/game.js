e67a9cb4f02b05c4639d0c28943020c0b96aaa3d
KDaryl <c00207452@itcarlow.ie>
2019-01-21 16:06:05 +0000

    Fixing errors, including corretc module files

--- src/game.js
+++ src/game.js
@@ -2,10 +2,18 @@
 class Game {
   constructor() {
     this.prevDt = Date.now();
-    this.canvas = new Canvas("main-canvas", 1920, 1080);
+    this.canvas = document.createElement("canvas");
+    this.canvas.id = 'mycanvas';
+    this.canvas.width = window.innerWidth;
+    this.canvas.height = window.innerHeight;
+    this.ctx = this.canvas.getContext("2d");
+    document.body.appendChild(this.canvas);
 
     //Creating the Menu Manager
     this.mManager = new MenuManager();
+
+    //Need to add a scene to the menu manager to stop the error
+  }
 
   run() {
     this.loop();
@@ -27,8 +35,6 @@ class Game {
   }
 
   render() {
-    this.canvas.context2D.drawImage(this.bgImg, 0, 0);
-
     //Call draw on the menu manager and pass the context over as canvas is not needed?
     this.mManager.draw(this.canvas.context2D);
   }
