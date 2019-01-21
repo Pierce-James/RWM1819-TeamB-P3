678e339fe0f5857339a955b58efb930377f0c8f4
KDaryl <c00207452@itcarlow.ie>
2019-01-21 16:20:38 +0000

    Added base game scene class, removes menu manager error

---
+++ src/Scenes/game_scene.js
@@ -0,0 +1,14 @@
+
+class GameScene {
+  constructor() {
+    //Add game objects here, player, ghosts etc.
+  }
+
+  update(dt) {
+    //Update game objects, pass dt to time related objects (things that move)
+  }
+
+  draw(ctx) {
+    //Draw using ctx
+  }
+}
