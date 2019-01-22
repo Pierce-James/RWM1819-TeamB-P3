4e77c7ee4113ba826277be73b6e1f1cc46e0b5e9
Pierce-James <C00203711@itcarlow.ie>
2019-01-22 16:27:15 +0000

    Added map and tile class. Tilemap rendering

--- src/game.js
+++ src/game.js
@@ -8,10 +8,11 @@ class Game {
     this.canvas.height = window.innerHeight;
     this.ctx = this.canvas.getContext("2d");
     document.body.appendChild(this.canvas);
-
+    document.body.style.backgroundColor = "#000000";
     //Creating the Menu Manager
-    this.mManager = new MenuManager();
-    this.mManager.addScene("Game Scene", new GameScene());
+    //this.mManager = new MenuManager();
+    //this.mManager.addScene("Game Scene", new GameScene());
+    this.map = new Map("src/tilemap.json");
     //Need to add a scene to the menu manager to stop the error
   }
 
@@ -31,12 +32,13 @@ class Game {
     const dt = this.calculateDt();
 
     //Update the menu manager and pass in dt for any time related functions
-    this.mManager.update(dt);
+    //this.mManager.update(dt);
   }
 
   render() {
     //Call draw on the menu manager and pass the context over as canvas is not needed?
-    this.mManager.draw(this.ctx);
+   // this.mManager.draw(this.ctx);
+   this.map.render();
   }
 
   calculateDt() {
