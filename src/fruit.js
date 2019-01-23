da14c6106125862d4c796862146462f3edf5bc4c
zjacques <zachary.jacques160@gmail.com>
2019-01-23 14:12:55 +0000

    added the key and updated submodules

--- src/fruit.js
+++ src/fruit.js
@@ -24,6 +24,22 @@ class Fruit
                 this.value = 700;
                 img.src = "ASSETS/SPRITES/Apple_72.png";
                 break;
+            case 'melon':
+                this.value = 1000;
+                img.src = "ASSETS/SPRITES/Melon_72.png";
+                break;
+            case 'galaxian':
+                this.value = 2000;
+                img.src = "ASSETS/SPRITES/Galaxian_Boss_72.png";
+                break;
+            case 'bell':
+                this.value = 3000;
+                img.src = "ASSETS/SPRITES/Bell_72.png";
+                break;
+            case 'key':
+                this.value = 5000;
+                img.src = "ASSETS/SPRITES/Key_72.png";
+                break;
         }
         this.spr = new Sprite(this.x, this.y, 32, 32, img, 32, 32);
 
