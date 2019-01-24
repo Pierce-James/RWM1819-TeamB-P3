13a818ec0132dbc03d54dfb0607dccaf9f6351db
KDaryl <c00207452@itcarlow.ie>
2019-01-24 14:14:25 +0000

    Added vunerabilty sprites and method to make the ghosts vunerable

--- src/Scenes/game_scene.js
+++ src/Scenes/game_scene.js
@@ -6,10 +6,10 @@ class GameScene {
 
     this.player = new Player(32, 32, this.tileMap);
     this.isActive = false;
-    this.blinkyGhost = new Ghost("Blinky", 384, 416, this.tileMap, new Vector2(25,1)); //Scatters to top right
+    this.blinkyGhost = new Ghost("Blinky", 384, 416, this.tileMap, new Vector2(26,1)); //Scatters to top right
     this.pinkyGhost = new Ghost("Pinky", 448, 416, this.tileMap, new Vector2(1,1)); //Scatters to top left
     this.clydeGhost = new Ghost("Clyde", 448, 384, this.tileMap, new Vector2(1,29)); //Scatters to bottom left
-    this.inkyGhost = new Ghost("Inky", 448, 384, this.tileMap, new Vector2(25,29)); //Scatters to bottom right
+    this.inkyGhost = new Ghost("Inky", 448, 384, this.tileMap, new Vector2(26,29)); //Scatters to bottom right
     this.inkyGhost.blinkyRef = this.blinkyGhost; //Set the reference to the blinky ghost
 
     this.topBar = new topUI();
